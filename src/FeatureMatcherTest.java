d0ab981e19d3a569a293e133c92bcb7e5ad5b378
Cole Savage <colesavage02@gmail.com>
2018-12-28 12:45:58 -0800

    Added custom opencv install with the contrib modules and created a test program for the opencv Retina class.

--- src/FeatureMatcherTest.java
+++ src/FeatureMatcherTest.java
@@ -3,6 +3,7 @@ import org.opencv.core.*;
 import org.opencv.features2d.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.xfeatures2d.*;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -16,8 +17,8 @@ import java.util.List;
 public class FeatureMatcherTest {
     static {System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
     public static void main(String args[]) {
-        String templateName = "C:\\Users\\Cole Savage\\Desktop\\Data\\2.jpg";
-        String sceneName = "C:\\Users\\Cole Savage\\Desktop\\Data\\unnamed.jpg";
+        String templateName = "C:\\Users\\coles\\OneDrive\\Desktop\\Data\\2.jpg";
+        String sceneName = "C:\\Users\\coles\\OneDrive\\Desktop\\Data\\unnamed.jpg";
         Mat template = Imgcodecs.imread(templateName,Imgcodecs.IMREAD_GRAYSCALE);
         Mat scene = Imgcodecs.imread(sceneName,Imgcodecs.IMREAD_GRAYSCALE);
         /*Mat scene = new Mat();
@@ -37,7 +38,7 @@ public class FeatureMatcherTest {
         } catch (InterruptedException ex) {
         }
 
-        cap.read(nvghffhkjgfvhmnhngmhnjmgghnmghjgjhgjhgjhghjscene);
+        cap.read(scene);
         showResult(scene);
         Imgproc.cvtColor(scene,scene,Imgproc.COLOR_BGR2GRAY);
 
